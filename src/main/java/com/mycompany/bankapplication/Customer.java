e376ba5b8974d48b56640ab45c8c698e0a0732e9
jamesganley <jamesganley96@gmail.com>
2019-11-21 12:08:43 +0000

    variables and methods made for the customer class

--- src/main/java/com/mycompany/bankapplication/Customer.java
+++ src/main/java/com/mycompany/bankapplication/Customer.java
@@ -7,8 +7,60 @@ package com.mycompany.bankapplication;
 
 /**
  *
- * @author blras
+ * @author Cobalt Gang
  */
+
+//Name char
+//Address VarChar
+//Eircode VarChar
+//Email Char
+//BIC VarChar
 public class Customer {
+    private String Name;
+    private String Address;
+    private String Eircode;
+    private String Email;
+    private String BIC;
+    
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getEircode() {
+        return Eircode;
+    }
+
+    public void setEircode(String Eircode) {
+        this.Eircode = Eircode;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getBIC() {
+        return BIC;
+    }
+
+    public void setBIC(String BIC) {
+        this.BIC = BIC;
+    }
+   
     
 }
