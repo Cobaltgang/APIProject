df0dda24be3c41bc6f69a28b62312b4b54f0e518
Eoin Kirwan <eoinkirwan@Eoins-MacBook-Pro.local>
2019-12-03 12:27:43 +0000

    d

--- src/main/java/com/mycompany/bankapplication/Account.java
+++ src/main/java/com/mycompany/bankapplication/Account.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,6 +9,7 @@ package com.mycompany.bankapplication;
  *
  * @author blras
  */
+
 public class Account {
     
 }
--- src/main/java/com/mycompany/bankapplication/Transaction.java
+++ src/main/java/com/mycompany/bankapplication/Transaction.java
@@ -5,10 +5,28 @@
  */
 package com.mycompany.bankapplication;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
+
 /**
  *
  * @author blras
  */
+
+/* Account class
+Transaction type - credit or debit
+date 
+amount in user account
+balance after transaction
+
+make transaction ()
+
+*/
 public class Transaction {
+    
+    private String transActionType;
+    private String dateTime;
+    private Double balance;
+    private Double newBalance;
+    private int 
     
 }
